0a85b1fb2ab40e8521ab84e96f21bc1758b528a9
twinmagix <sazindwandwe1997@gmail.com>
2022-05-05 15:26:39 +0200

    first commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: pointer to the list.
  * Return: number of nodes.
  **/
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
 	size_t cont = 0;
 
 	while (node)
 	{
-		printf("%i\n", node->n);
 		cont++;
 		node = node->next;
 	}
