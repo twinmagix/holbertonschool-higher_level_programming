d442892d12551a42f94092bae7a2871702b49d62
Sazi A. Ndwandwe <40753995+twinmagix@users.noreply.github.com>
2022-05-31 19:20:10 +0200

    input commit

--- 0x0B-python-input_output/4-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def append_write(filename="", text=""):
-    """
-    Write a function that appends a string at the end of a text file (UTF8)
-    and returns the number of characters added.
-    """
-    with open(filename, mode="a", encoding="utf-8") as myFile:
-        return myFile.write(str(text))
--- 0x0B-python-input_output/README.md
+++
@@ -1,1 +0,0 @@
-Python Input and Output!!!
